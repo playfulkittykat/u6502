864ce28ca029684d3dc7c32701fea8087fadc170
Playful KittyKat <pkk@tabby.rocks>
2025-11-10 12:26:32 -0500

    add ci

--- examples/lib1.c
+++ examples/lib1.c
@@ -5,135 +5,146 @@
 
 /* Emulated OS functions. */
 
-#define WRCH	0xFFEE	/* Write accumulator to stdout. */
+#define WRCH 0xFFEE /* Write accumulator to stdout. */
 
 /* Write the accumulator to stdout.  This function will be invoked
  * when the emulated program calls 0xFFEE.
  */
-static uint16_t wrch(U6502 *mpu)
+static uint16_t
+wrch(U6502* mpu)
 {
-  int pc;
+    int pc;
 
-  /* Write the character.
-   */
-  putchar(mpu->registers->a);
+    /* Write the character.
+     */
+    putchar(mpu->registers->a);
 
-  uint8_t *memory = (uint8_t *)mpu->userdata;
+    uint8_t* memory = (uint8_t*)mpu->userdata;
 
-  /* We arrived here from a JSR instruction.  The stack contains the
-   * saved PC.  Pop it off the stack.
-   */
-  pc  = memory[++mpu->registers->s + 0x100];
-  pc |= memory[++mpu->registers->s + 0x100] << 8;
+    /* We arrived here from a JSR instruction.  The stack contains the
+     * saved PC.  Pop it off the stack.
+     */
+    pc = memory[++mpu->registers->s + 0x100];
+    pc |= memory[++mpu->registers->s + 0x100] << 8;
 
-  /* The JSR instruction pushes the value of PC before it has been
-   * incremented to point to the instruction after the JSR.  Return PC
-   * + 1 as the address for the next insn.  Returning non-zero
-   * indicates that we handled the 'subroutine' ourselves, and the
-   * emulator should pretend the original 'JSR' neveer happened at
-   * all.
-   */
-  return pc + 1;  /* JSR pushes next insn addr - 1 */
+    /* The JSR instruction pushes the value of PC before it has been
+     * incremented to point to the instruction after the JSR.  Return PC
+     * + 1 as the address for the next insn.  Returning non-zero
+     * indicates that we handled the 'subroutine' ourselves, and the
+     * emulator should pretend the original 'JSR' neveer happened at
+     * all.
+     */
+    return pc + 1; /* JSR pushes next insn addr - 1 */
 }
-
 
 /* Exit gracefully.  We arrange for this function to be called when
  * the emulator tries to transfer control to address 0.
  */
-static uint16_t done(U6502 *mpu)
+static uint16_t
+done(U6502* mpu)
 {
-  char buffer[64];
+    char buffer[64];
 
-  /* Dump the internal state of the processor.
-   */
-  U6502_dump(mpu, buffer);
+    /* Dump the internal state of the processor.
+     */
+    U6502_dump(mpu, buffer);
 
-  /* Print a cute message and quit.
-   */
-  printf("\nBRK instruction\n%s\n", buffer);
-  exit(0);
+    /* Print a cute message and quit.
+     */
+    printf("\nBRK instruction\n%s\n", buffer);
+    exit(0);
 }
 
-static uint16_t call(U6502 *mpu, uint16_t address, uint16_t source) {
-  (void)source;
-
-  switch (address) {
-    case 0:
-      return done(mpu);
-    case WRCH:
-      return wrch(mpu);
-    default:
-      return 0;
-  }
-}
-
-static uint8_t read(U6502 *mpu, uint16_t address) {
-  return ((uint8_t *)mpu->userdata)[address];
-}
-
-static void write(U6502 *mpu, uint16_t address, uint8_t value) {
-  ((uint8_t *)mpu->userdata)[address] = value;
-}
-
-int main(void)
+static uint16_t
+call(U6502* mpu, uint16_t address, uint16_t source)
 {
-  uint8_t *memory = calloc(0x10000, 1);
-  if (!memory) {
-    abort();
-  }
+    (void)source;
 
-  /* Install the callback functions defined above.
-   */
-  U6502_Callbacks callbacks = {
-    .call = call,
-    .read = read,
-    .write = write,
-  };
+    switch (address) {
+        case 0:
+            return done(mpu);
+        case WRCH:
+            return wrch(mpu);
+        default:
+            return 0;
+    }
+}
 
-  U6502    *mpu = U6502_new(0, callbacks, &memory[0]);	/* Make a 6502 */
-  unsigned  pc  = 0x1000;		/* PC for 'assembly' */
+static uint8_t
+read(U6502* mpu, uint16_t address)
+{
+    return ((uint8_t*)mpu->userdata)[address];
+}
 
-  /* A few macros that dump bytes into the 6502's memory.
-   */
-# define gen1(X)	(memory[pc++]= (uint8_t)(X))
-# define gen2(X,Y)	gen1(X); gen1(Y)
-# define gen3(X,Y,Z)	gen1(X); gen2(Y,Z)
+static void
+write(U6502* mpu, uint16_t address, uint8_t value)
+{
+    ((uint8_t*)mpu->userdata)[address] = value;
+}
 
-  /* Hand-assemble the program.
-   */
-  gen2(0xA2, 'A'     );	// LDX #'A'
-  gen1(0x8A          );	// TXA
-  gen3(0x20,0xEE,0xFF);	// JSR FFEE
-  gen1(0xE8          );	// INX
-  gen2(0xE0, 'Z'+1   );	// CPX #'Z'+1
-  gen2(0xD0, -9      );	// BNE 0x1002
-  gen2(0xA9, '\n'    );	// LDA #'\n'
-  gen3(0x20,0xEE,0xFF);	// JSR FFEE
-  gen2(0x00,0x00     ); // BRK
+int
+main(void)
+{
+    uint8_t* memory = calloc(0x10000, 1);
+    if (!memory) {
+        abort();
+    }
 
-  /* Just for fun: disssemble the program.
-   */
-  {
-    char     insn[64];
-    uint16_t ip= 0x1000;
-    while (ip < pc)
-      {
-	ip += U6502_disassemble(mpu, ip, insn);
-	printf("%04X %s\n", ip, insn);
-      }
-  }
+    /* Install the callback functions defined above.
+     */
+    U6502_Callbacks callbacks = {
+        .call = call,
+        .read = read,
+        .write = write,
+    };
 
-  /* Point the RESET vector at the first instruction in the assembled
-   * program.
-   */
-  U6502_setVector(mpu, RST, 0x1000);
+    U6502* mpu = U6502_new(0, callbacks, &memory[0]); /* Make a 6502 */
+    unsigned pc = 0x1000;                             /* PC for 'assembly' */
 
-  /* Reset the 6502 and run the program.
-   */
-  U6502_reset(mpu);
-  U6502_run(mpu);
-  U6502_delete(mpu);	/* We never reach here, but what the hey. */
-  free(memory);
+    /* A few macros that dump bytes into the 6502's memory.
+     */
+#define gen1(X) (memory[pc++] = (uint8_t)(X))
+#define gen2(X, Y)                                                             \
+    gen1(X);                                                                   \
+    gen1(Y)
+#define gen3(X, Y, Z)                                                          \
+    gen1(X);                                                                   \
+    gen2(Y, Z)
 
-  return 0;
+    /* Hand-assemble the program.
+     */
+    gen2(0xA2, 'A');        // LDX #'A'
+    gen1(0x8A);             // TXA
+    gen3(0x20, 0xEE, 0xFF); // JSR FFEE
+    gen1(0xE8);             // INX
+    gen2(0xE0, 'Z' + 1);    // CPX #'Z'+1
+    gen2(0xD0, -9);         // BNE 0x1002
+    gen2(0xA9, '\n');       // LDA #'\n'
+    gen3(0x20, 0xEE, 0xFF); // JSR FFEE
+    gen2(0x00, 0x00);       // BRK
+
+    /* Just for fun: disssemble the program.
+     */
+    {
+        char insn[64];
+        uint16_t ip = 0x1000;
+        while (ip < pc) {
+            ip += U6502_disassemble(mpu, ip, insn);
+            printf("%04X %s\n", ip, insn);
+        }
+    }
+
+    /* Point the RESET vector at the first instruction in the assembled
+     * program.
+     */
+    U6502_setVector(mpu, RST, 0x1000);
+
+    /* Reset the 6502 and run the program.
+     */
+    U6502_reset(mpu);
+    U6502_run(mpu);
+    U6502_delete(mpu); /* We never reach here, but what the hey. */
+    free(memory);
+
+    return 0;
 }
--- include/u6502.h
+++ include/u6502.h
@@ -1,87 +1,103 @@
 #ifndef __u6502_h
 #define __u6502_h
 
-
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef struct _U6502		U6502;
-typedef struct _U6502_Registers	U6502_Registers;
-typedef struct _U6502_Callbacks	U6502_Callbacks;
+    typedef struct _U6502 U6502;
+    typedef struct _U6502_Registers U6502_Registers;
+    typedef struct _U6502_Callbacks U6502_Callbacks;
 
-typedef uint8_t  (*U6502_ReadCallback)(U6502 *mpu, uint16_t address);
-typedef void     (*U6502_WriteCallback)(U6502 *mpu, uint16_t address, uint8_t data);
-typedef uint16_t (*U6502_CallCallback)(U6502 *mpu, uint16_t address, uint16_t source);
+    typedef uint8_t (*U6502_ReadCallback)(U6502* mpu, uint16_t address);
+    typedef void (*U6502_WriteCallback)(U6502* mpu,
+                                        uint16_t address,
+                                        uint8_t data);
+    typedef uint16_t (*U6502_CallCallback)(U6502* mpu,
+                                           uint16_t address,
+                                           uint16_t source);
 
-enum {
-  U6502_NMIVector= 0xfffa,  U6502_NMIVectorLSB= 0xfffa,  U6502_NMIVectorMSB= 0xfffb,
-  U6502_RSTVector= 0xfffc,  U6502_RSTVectorLSB= 0xfffc,  U6502_RSTVectorMSB= 0xfffd,
-  U6502_IRQVector= 0xfffe,  U6502_IRQVectorLSB= 0xfffe,  U6502_IRQVectorMSB= 0xffff
-};
+    enum
+    {
+        U6502_NMIVector = 0xfffa,
+        U6502_NMIVectorLSB = 0xfffa,
+        U6502_NMIVectorMSB = 0xfffb,
+        U6502_RSTVector = 0xfffc,
+        U6502_RSTVectorLSB = 0xfffc,
+        U6502_RSTVectorMSB = 0xfffd,
+        U6502_IRQVector = 0xfffe,
+        U6502_IRQVectorLSB = 0xfffe,
+        U6502_IRQVectorMSB = 0xffff
+    };
 
-enum _U6502_Status {
-  U6502_StatusOk= 0,
-  U6502_StatusStack,
-  U6502_StatusIll,
-};
+    enum _U6502_Status
+    {
+        U6502_StatusOk = 0,
+        U6502_StatusStack,
+        U6502_StatusIll,
+    };
 
-typedef enum _U6502_Status U6502_Status;
+    typedef enum _U6502_Status U6502_Status;
 
-struct _U6502_Registers
-{
-  uint8_t   a;	/* accumulator */
-  uint8_t   x;	/* X index register */
-  uint8_t   y;	/* Y index register */
-  uint8_t   p;	/* processor status register */
-  uint8_t   s;	/* stack pointer */
-  uint16_t pc;	/* program counter */
-};
+    struct _U6502_Registers
+    {
+        uint8_t a;   /* accumulator */
+        uint8_t x;   /* X index register */
+        uint8_t y;   /* Y index register */
+        uint8_t p;   /* processor status register */
+        uint8_t s;   /* stack pointer */
+        uint16_t pc; /* program counter */
+    };
 
-struct _U6502_Callbacks
-{
-  U6502_ReadCallback read;
-  U6502_WriteCallback write;
-  U6502_CallCallback call;
-};
+    struct _U6502_Callbacks
+    {
+        U6502_ReadCallback read;
+        U6502_WriteCallback write;
+        U6502_CallCallback call;
+    };
 
-struct _U6502
-{
-  U6502_Registers *registers;
-  U6502_Callbacks callbacks;
-  unsigned int	   flags;
-  unsigned char    ticks;
-  void* userdata;
-};
+    struct _U6502
+    {
+        U6502_Registers* registers;
+        U6502_Callbacks callbacks;
+        unsigned int flags;
+        unsigned char ticks;
+        void* userdata;
+    };
 
-enum {
-  U6502_RegistersAllocated = 1 << 0,
-};
+    enum
+    {
+        U6502_RegistersAllocated = 1 << 0,
+    };
 
-extern U6502 *U6502_new(U6502_Registers *registers, U6502_Callbacks callbacks, void* userdata);
-extern void   U6502_reset(U6502 *mpu);
-extern void   U6502_nmi(U6502 *mpu);
-extern void   U6502_irq(U6502 *mpu);
-extern U6502_Status   U6502_run(U6502 *mpu);
-extern U6502_Status   U6502_tick(U6502 *mpu);
-extern U6502_Status   U6502_step(U6502 *mpu);
-extern int    U6502_disassemble(U6502 *mpu, uint16_t addr, char buffer[64]);
-extern void   U6502_dump(U6502 *mpu, char buffer[64]);
-extern void   U6502_delete(U6502 *mpu);
+    extern U6502* U6502_new(U6502_Registers* registers,
+                            U6502_Callbacks callbacks,
+                            void* userdata);
+    extern void U6502_reset(U6502* mpu);
+    extern void U6502_nmi(U6502* mpu);
+    extern void U6502_irq(U6502* mpu);
+    extern U6502_Status U6502_run(U6502* mpu);
+    extern U6502_Status U6502_tick(U6502* mpu);
+    extern U6502_Status U6502_step(U6502* mpu);
+    extern int U6502_disassemble(U6502* mpu, uint16_t addr, char buffer[64]);
+    extern void U6502_dump(U6502* mpu, char buffer[64]);
+    extern void U6502_delete(U6502* mpu);
 
-#define U6502_getVector(MPU, VEC)			\
-  ( ( ((MPU)->callbacks.read((MPU), U6502_##VEC##VectorLSB)) )		\
-    | ((MPU)->callbacks.read((MPU), U6502_##VEC##VectorMSB) << 8) )
+#define U6502_getVector(MPU, VEC)                                              \
+    ((((MPU)->callbacks.read((MPU), U6502_##VEC##VectorLSB))) |                \
+     ((MPU)->callbacks.read((MPU), U6502_##VEC##VectorMSB) << 8))
 
-#define U6502_setVector(MPU, VEC, ADDR)						\
-  ( ( ((MPU)->callbacks.write((MPU), U6502_##VEC##VectorLSB, ((uint8_t)(ADDR)) & 0xff)) )	\
-    , ((MPU)->callbacks.write((MPU), U6502_##VEC##VectorMSB, (uint8_t)((ADDR) >> 8))) )
+#define U6502_setVector(MPU, VEC, ADDR)                                        \
+    ((((MPU)->callbacks.write(                                                 \
+       (MPU), U6502_##VEC##VectorLSB, ((uint8_t)(ADDR)) & 0xff))),             \
+     ((MPU)->callbacks.write(                                                  \
+       (MPU), U6502_##VEC##VectorMSB, (uint8_t)((ADDR) >> 8))))
 
-#define U6502_getCallback(MPU, TYPE)	((MPU)->callbacks.TYPE)
-#define U6502_setCallback(MPU, TYPE, FN)	((MPU)->callbacks.TYPE= (FN))
-
+#define U6502_getCallback(MPU, TYPE) ((MPU)->callbacks.TYPE)
+#define U6502_setCallback(MPU, TYPE, FN) ((MPU)->callbacks.TYPE = (FN))
 
 #ifdef __cplusplus
 }
